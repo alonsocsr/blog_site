ccd86c6c6a9bd40acf98a052ee0272bb39502fee
césar <cesaralonso99@gmail.com>
2024-08-06 16:10:48 -0400

    models & views updated from db

--- blog/views.py
+++ blog/views.py
@@ -1,83 +1,23 @@
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
-
-all_posts = [
-    {
-        "slug": "the-wall",
-        "image": "The_Wall_from_the_south.webp",
-        "author": "César",
-        "date": date(2024, 7, 29),
-        "title": "The wall",
-        "excerpt": "The Wall is a colossal fortification which stretches for 100 leagues (300 miles or 482.8 kilometers) along the northern border of the Kingdom of the North, separating the realm from the domain of the wildlings who live beyond.",
-        "content": """
-            The Wall is a colossal fortification which stretches for 100 leagues (300 miles or 482.8 kilometers) along the northern border of the Kingdom of the North, separating the realm from the domain of the wildlings who live beyond. 
-            The Wall is reported to be over 700 feet tall and is made of solid ice. It was supposedly constructed using both magic and mundane means some eight millennia ago in the aftermath of the Long Night to defend the realm against the White Walkers who dwell in the far north, though they are now considered myths by most. The Wall has grown considerably since it was first constructed, as the Night's Watch has spent the years augmenting it with ice blocks and upgraded defenses.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.png",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-def get_date(post):
-    return post['date']
+from .models import Post
 
 # Create your views here.
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_post = sorted_posts[-3:]
+    latest_post = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_post
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post
     })
