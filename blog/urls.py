69c429f0edbf82ea9102acaecd0fdc15237a09ba
césar <cesaralonso99@gmail.com>
2024-08-11 17:10:27 -0400

    allpost and detail-post to class view

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
+    path("posts", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
 ]
